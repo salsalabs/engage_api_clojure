bc5d38688eb9fcd6c6dbb8d1d56685ec3fc94099
Allen Leonard <aleonard@salsalabs.com>
2022-04-22 13:58:45 -0500

    Removed app that was not ready.  Branch next time!

--- blast_recipients/doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to blast_recipients
-
-TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
--- blast_recipients/test/engage_segment_xref/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns engage-segment-xref.core-test
-  (:require [clojure.test :refer :all]
-            [engage-segment-xref.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
